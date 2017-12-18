4062f0bf0ef39173f27b8518d0c9696341ddc51a
lusyerit <lusyerit@gmail.com>
2017-12-18 15:12:47 +0400

    first commit

--- src/pogeobject/TestClass.java
+++ src/pogeobject/TestClass.java
@@ -2,6 +2,6 @@ package pogeobject;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.print("sdfsdg");
+        System.out.print("sdfsgjjhfgjdg");
     }
 }
